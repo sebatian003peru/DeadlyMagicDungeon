d5f4d00b98bc69b7edd05b98c88132cd3b55e9c0
sebatian003peru <sebastian003peru@gmail.com>
2018-04-30 10:42:32 -0500

    Se agregan Menus

---
+++ MagicDeadlyDungeon/Assets/Scripts/Menu/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+	public string sceneName;
+
+	public void Load()
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+
+}
